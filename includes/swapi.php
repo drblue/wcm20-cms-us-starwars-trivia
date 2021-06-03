73365421c40e7f77a2ca3286e10b6ba10e5525bf
Johan Nordström <johan@digitalvillage.se>
2021-06-03 12:46:34 +0200

    Implemented caching using WordPress Transients.

--- includes/swapi.php
+++ includes/swapi.php
@@ -4,13 +4,15 @@
  */
 
 /**
- * Get data from a SWAPI endpoint
+ * Get data from a JSON API
  *
- * @param string $endpoint
+ * @param string $url
  * @return array
  */
-function swapi_get($endpoint) {
-	$response = wp_remote_get("https://swapi.dev/api/{$endpoint}");
+function swapi_get_url($url) {
+	$response = wp_remote_get($url, [
+		'timeout' => 30,
+	]);
 
 	if (wp_remote_retrieve_response_code($response) !== 200) {
 		return [
@@ -28,13 +30,39 @@ function swapi_get($endpoint) {
 	];
 }
 
+
+/**
+ * Get (possibly cached) data from a SWAPI endpoint
+ *
+ * @param string $endpoint
+ * @return array
+ */
+function swapi_get($endpoint) {
+	// Get transient for this endpoint
+	$res = get_transient("swapi_{$endpoint}");
+
+	// Did (valid) cached data exist for this endpoint?
+	if ($res === false) {
+		// Cached data didn't exist or had expired for the endpoint
+		$res = swapi_get_url("https://swapi.dev/api/{$endpoint}");
+
+		// Fake a slow api 😈
+		// sleep(5);
+
+		// Store retrieved data in the cache for 4 hours
+		set_transient("swapi_{$endpoint}", $res, 4 * HOUR_IN_SECONDS);
+	}
+
+	return $res;
+}
+
 /**
  * Get films
  *
  * @return array
  */
 function swapi_get_films() {
-	return swapi_get("films/");
+	return swapi_get("films");
 }
 
 /**
@@ -43,7 +71,7 @@ function swapi_get_films() {
  * @return array
  */
 function swapi_get_people() {
-	return swapi_get("people/");
+	return swapi_get("people");
 }
 
 /**
